260b7b5248f193bb144ca86b88e0729aa928fa3e
Valentin Rüeck <valentin.rueeck@diva-e.com>
2018-06-07 12:26:49 +0200

    refactoring

--- app/src/main/java/com/example/vrueeck/arcore_ir/ARContentCreator.java
+++ app/src/main/java/com/example/vrueeck/arcore_ir/ARContentCreator.java
@@ -1,10 +1,16 @@
 package com.example.vrueeck.arcore_ir;
 
 import android.content.Context;
+import android.graphics.ColorSpace;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.ar.core.AugmentedImage;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.MaterialFactory;
@@ -17,20 +23,130 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class ARContentCreator {
+    private static AudioContentController audioContentController;
 
-    public static ModelRenderable createHighlightImagePlane(AugmentedImage image, Context context) {
+    public static void addHighlightImagePlane(Context context, AnchorNode anchorNode, AugmentedImage image) {
         Vector3 size = new Vector3(image.getExtentX(),image.getExtentZ(), -0.01f);
         Vector3 center = new Vector3(0,0,0);
-        final Map<String, ModelRenderable> modelRenderableHashMap = new HashMap<>();
 
 
         MaterialFactory.makeTransparentWithColor(context,new Color(1,1,1,0.5f))
                 .thenAccept(
                         material -> {
-                            modelRenderableHashMap.put("highlightPlane",ShapeFactory.makeCube(size, center, material));
+                            ModelRenderable highlightPlane = ShapeFactory.makeCube(size, center, material);
+                            Node highlightPlaneNode = new Node();
+                            highlightPlaneNode.setName("highlightPlaneNode");
+                            highlightPlaneNode.setRenderable(highlightPlane);
+                            highlightPlaneNode.setLocalRotation(new Quaternion(new Vector3(1,0,0), 90));
+                            highlightPlaneNode.setParent(anchorNode);
+                            MainActivity.removeHighlightNode(anchorNode, highlightPlaneNode);
                         });
+    }
 
-        return modelRenderableHashMap.get("highlightPlane");
+    public static void addDescriptionPlane(Context context, AnchorNode anchorNode, AugmentedImage image){
+        CompletableFuture<ViewRenderable> future = ViewRenderable.builder().setView(context, R.layout.text_view).build();
+        future.thenAccept( view -> {
+            int height = Math.round(view.getPixelsToMetersRatio() * 0.25f);
+            int width = Math.round(view.getPixelsToMetersRatio() * image.getExtentX());
+
+            TextView headline = view.getView().findViewById(R.id.textViewHeadline);
+            SpannableString headlineText = new SpannableString(image.getName());
+            headlineText.setSpan(new UnderlineSpan(),0, headlineText.length(),0);
+            headline.setText(headlineText);
+
+            TextView descriptionText = view.getView().findViewById(R.id.textView);
+            descriptionText.setText(PaintingDescriptionTextRetriever.retrieveDescriptionText(image.getName()));
+            descriptionText.setWidth(width);
+            descriptionText.setHeight(height);
+
+            Node descriptionPlaneNode = new Node();
+            descriptionPlaneNode.setName("descriptionPlaneNode");
+            descriptionPlaneNode.setRenderable(view);
+            descriptionPlaneNode.setParent(anchorNode);
+            descriptionPlaneNode.setLocalPosition(new Vector3(0f,0f, image.getExtentZ() + descriptionText.getHeight() + 0.3f));
+            descriptionPlaneNode.setLocalRotation(new Quaternion(new Vector3(1,0,0), -90));
+        });
+    }
+
+    public static void addInfoButton(Context context,AnchorNode anchorNode, AugmentedImage image) {
+        ModelRenderable.builder()
+                .setSource(context, R.raw.infobutton)
+                .build()
+                .thenAccept( infoButton -> {
+                    Node infoButtonNode = new Node();
+                    infoButtonNode.setName("infoButtonNode");
+                    infoButtonNode.setRenderable(infoButton);
+                    infoButtonNode.setWorldScale(new Vector3(0.005f,0.005f,0.005f));
+                    infoButtonNode.setLocalRotation(new Quaternion(new Vector3(1f,0f,0f), -90));
+                    infoButtonNode.setParent(anchorNode);
+                    infoButtonNode.setLocalPosition(new Vector3(0f,0f, -image.getExtentZ() / 2 - infoButtonNode.getWorldScale().x / 2));
+                    infoButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
+                        Log.d("TAP: ", "infoButtonNode tapped");
+                        ARContentCreator.addDescriptionPlane(context, anchorNode, image);
+                    });
+                })
+                .exceptionally(
+                        throwable -> {
+                            Log.e("RRR", "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+    }
+
+    public static void addPlayButton(Context context, AnchorNode anchorNode, AugmentedImage image){
+        ModelRenderable.builder()
+                .setSource(context, R.raw.playbutton)
+                .build()
+                .thenAccept( playButton -> {
+                    Node playButtonNode = new Node();
+                    playButtonNode.setName("playButtonNode");
+                    playButtonNode.setRenderable(playButton);
+                    playButtonNode.setWorldScale(new Vector3(0.002f,0.002f,0.002f));
+                    playButtonNode.setLocalRotation(new Quaternion(new Vector3(0f,1f,0f), -90));
+                    playButtonNode.setParent(anchorNode);
+                    playButtonNode.setLocalPosition(new Vector3(image.getExtentX() / 2 + playButtonNode.getWorldScale().x, 0f, 0f));
+                    playButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
+                        audioContentController = new AudioContentController(context, getAudioFileResId(context, image.getName().toLowerCase()));
+                        audioContentController.playAudio();
+                        audioContentController.getMediaPlayer().setOnCompletionListener(mp -> addPlayButton(context, anchorNode,image));
+                        anchorNode.removeChild(playButtonNode);
+                        ARContentCreator.addPauseButtonNode(context, anchorNode, image);
+                    });
+                })
+                .exceptionally(
+                        throwable -> {
+                            Log.e("RRR", "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+    }
+
+    public static void addPauseButtonNode(Context context, AnchorNode anchorNode, AugmentedImage image){
+        ModelRenderable.builder()
+                .setSource(context, R.raw.pausebutton)
+                .build()
+                .thenAccept( pauseButton -> {
+                    Node pauseButtonNode = new Node();
+                    pauseButtonNode.setName("pauseButtonNode");
+                    pauseButtonNode.setRenderable(pauseButton);
+                    pauseButtonNode.setWorldScale(new Vector3(0.002f,0.002f,0.002f));
+                    pauseButtonNode.setLocalRotation(new Quaternion(new Vector3(0f,1f,0f), -90));
+                    pauseButtonNode.setParent(anchorNode);
+                    pauseButtonNode.setLocalPosition(new Vector3(image.getExtentX() / 2 + pauseButtonNode.getWorldScale().x / 2, 0f, 0f));
+                    pauseButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
+                        audioContentController.stopAudio();
+                        anchorNode.removeChild(pauseButtonNode);
+                        ARContentCreator.addPlayButton(context, anchorNode, image);
+
+                    });
+                })
+                .exceptionally(
+                        throwable -> {
+                            Log.e("RRR", "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+    }
+
+    private static int getAudioFileResId(Context context, String imageName){
+        return context.getResources().getIdentifier(imageName + "_audio", "raw", context.getPackageName());
     }
 
 }
--- app/src/main/java/com/example/vrueeck/arcore_ir/MainActivity.java
+++ app/src/main/java/com/example/vrueeck/arcore_ir/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.vrueeck.arcore_ir;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     private AugmentedImageDatabase augmentedImageDatabase;
     private Config arConfig;
     private ArSceneView arSceneView;
-    private Handler handler = new Handler();
+//    private Handler handler = new Handler();
     private Collection<AugmentedImage> lastUpdatedAugmentedImages;
     private AudioContentController audioContentController;
 
@@ -103,28 +104,21 @@ public class MainActivity extends AppCompatActivity {
                 anchorNode.setName(image.getName() + "_anchorNode");
                 anchorNode.setParent(arSceneView.getScene());
 
-                ModelRenderable highlightPlane = ARContentCreator.createHighlightImagePlane(image, context);
-                Node highlightPlaneNode = new Node();
-                highlightPlaneNode.setName("highlightPlaneNode");
-                highlightPlaneNode.setRenderable(highlightPlane);
-                highlightPlaneNode.setLocalRotation(new Quaternion(new Vector3(1,0,0), 90));
-                highlightPlaneNode.setParent(anchorNode);
-                removeHighlightNode(anchorNode, highlightPlaneNode);
-
-                addInfoButton(anchorNode, image);
-                addPlayButton(anchorNode, image);
+                ARContentCreator.addHighlightImagePlane(context, anchorNode, image);
+                ARContentCreator.addInfoButton(context, anchorNode, image);
+                ARContentCreator.addPlayButton(context, anchorNode, image);
             }
             Log.d("IMAGE", "Anchors: " + arSession.getAllAnchors().size());
         }
         lastUpdatedAugmentedImages = augmentedImages;
     }
 
-    private void removeHighlightNode(Node highlightNodeParent, Node highlightNode) {
+    public static void removeHighlightNode(Node highlightNodeParent, Node highlightNode) {
         new java.util.Timer().schedule(
                 new java.util.TimerTask() {
                     @Override
                     public void run() {
-                        handler.post(() -> {
+                        new Handler(Looper.getMainLooper()).post(() -> {
                             highlightNodeParent.removeChild(highlightNode);
                         });
                     }
@@ -151,111 +145,4 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-    private void addDescriptionPlane(AnchorNode anchorNode, AugmentedImage image){
-        CompletableFuture<ViewRenderable> future = ViewRenderable.builder().setView(context, R.layout.text_view).build();
-        future.thenAccept( view -> {
-            int height = Math.round(view.getPixelsToMetersRatio() * 0.25f);
-            int width = Math.round(view.getPixelsToMetersRatio() * image.getExtentX());
-
-            TextView headline = view.getView().findViewById(R.id.textViewHeadline);
-            SpannableString headlineText = new SpannableString(image.getName());
-            headlineText.setSpan(new UnderlineSpan(),0, headlineText.length(),0);
-            headline.setText(headlineText);
-
-            TextView descriptionText = view.getView().findViewById(R.id.textView);
-            descriptionText.setText(PaintingDescriptionTextRetriever.retrieveDescriptionText(image.getName()));
-            descriptionText.setWidth(width);
-            descriptionText.setHeight(height);
-
-            Node descriptionPlaneNode = new Node();
-            descriptionPlaneNode.setName("descriptionPlaneNode");
-            descriptionPlaneNode.setRenderable(view);
-            descriptionPlaneNode.setParent(anchorNode);
-            descriptionPlaneNode.setLocalPosition(new Vector3(0f,0f, image.getExtentZ() + descriptionText.getHeight() + 0.3f));
-            descriptionPlaneNode.setLocalRotation(new Quaternion(new Vector3(1,0,0), -90));
-        });
-    }
-
-    private void addInfoButton(AnchorNode anchorNode, AugmentedImage image) {
-        ModelRenderable.builder()
-                .setSource(this,R.raw.infobutton)
-                .build()
-                .thenAccept( infoButton -> {
-                    Node infoButtonNode = new Node();
-                    infoButtonNode.setName("infoButtonNode");
-                    infoButtonNode.setRenderable(infoButton);
-                    infoButtonNode.setWorldScale(new Vector3(0.005f,0.005f,0.005f));
-                    infoButtonNode.setLocalRotation(new Quaternion(new Vector3(1f,0f,0f), -90));
-                    infoButtonNode.setParent(anchorNode);
-                    infoButtonNode.setLocalPosition(new Vector3(0f,0f, -image.getExtentZ() / 2 - infoButtonNode.getWorldScale().x / 2));
-                    infoButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
-                        Log.d("TAP: ", "infoButtonNode tapped");
-                        addDescriptionPlane(anchorNode, image);
-                    });
-                })
-                .exceptionally(
-                        throwable -> {
-                            Log.e("RRR", "Unable to load Renderable.", throwable);
-                            return null;
-                        });
-    }
-
-    private void addPlayButton(AnchorNode anchorNode, AugmentedImage image){
-        ModelRenderable.builder()
-                .setSource(this,R.raw.playbutton)
-                .build()
-                .thenAccept( playButton -> {
-                    Node playButtonNode = new Node();
-                    playButtonNode.setName("playButtonNode");
-                    playButtonNode.setRenderable(playButton);
-                    playButtonNode.setWorldScale(new Vector3(0.002f,0.002f,0.002f));
-                    playButtonNode.setLocalRotation(new Quaternion(new Vector3(0f,1f,0f), -90));
-                    playButtonNode.setParent(anchorNode);
-                    playButtonNode.setLocalPosition(new Vector3(image.getExtentX() / 2 + playButtonNode.getWorldScale().x, 0f, 0f));
-                    playButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
-                        this.audioContentController = new AudioContentController(context, getAudioFileResId(image.getName().toLowerCase()));
-                        this.audioContentController.playAudio();
-                        this.audioContentController.getMediaPlayer().setOnCompletionListener(mp -> addPlayButton(anchorNode,image));
-                        anchorNode.removeChild(playButtonNode);
-                        addPauseButtonNode(anchorNode, image);
-                    });
-                })
-                .exceptionally(
-                        throwable -> {
-                            Log.e("RRR", "Unable to load Renderable.", throwable);
-                            return null;
-                        });
-    }
-
-    private void addPauseButtonNode(AnchorNode anchorNode, AugmentedImage image){
-        ModelRenderable.builder()
-                .setSource(this,R.raw.pausebutton)
-                .build()
-                .thenAccept( pauseButton -> {
-                    Node pauseButtonNode = new Node();
-                    pauseButtonNode.setName("pauseButtonNode");
-                    pauseButtonNode.setRenderable(pauseButton);
-                    pauseButtonNode.setWorldScale(new Vector3(0.002f,0.002f,0.002f));
-                    pauseButtonNode.setLocalRotation(new Quaternion(new Vector3(0f,1f,0f), -90));
-                    pauseButtonNode.setParent(anchorNode);
-                    pauseButtonNode.setLocalPosition(new Vector3(image.getExtentX() / 2 + pauseButtonNode.getWorldScale().x / 2, 0f, 0f));
-                    pauseButtonNode.setOnTapListener((hitTestResult, motionEvent) -> {
-                        this.audioContentController.stopAudio();
-                        anchorNode.removeChild(pauseButtonNode);
-                        addPlayButton(anchorNode, image);
-
-                    });
-                })
-                .exceptionally(
-                        throwable -> {
-                            Log.e("RRR", "Unable to load Renderable.", throwable);
-                            return null;
-                        });
-    }
-
-    private int getAudioFileResId(String imageName){
-        return getResources().getIdentifier(imageName + "_audio", "raw", getPackageName());
-    }
-
 }
